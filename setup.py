be449c900a5973637709e23f78d26c6d77fe4650
Kunal <singhkunalakhilesh2@gmail.com>
2025-11-10 00:00:12 +0530

    Project structure done venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Singh Kunal Akhilesh",
+    author_email="singhkunalakhilesh2@gmail.com",
+    packages=find_packages()
+)
